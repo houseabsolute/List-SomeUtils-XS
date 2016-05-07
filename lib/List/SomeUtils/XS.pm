ebda907ebc6eb5242a641486102c256d8c34ea49
Dave Rolsky <autarch@urth.org>
2016-05-07 09:22:22 -0500

    No need for BEGIN block

--- lib/List/SomeUtils/XS.pm
+++ lib/List/SomeUtils/XS.pm
@@ -4,41 +4,39 @@ use 5.006;
 use strict;
 use warnings;
 
-BEGIN {
-    our $VERSION = '0.53';
+our $VERSION = '0.53';
 
-    require XSLoader;
-    XSLoader::load( __PACKAGE__, $VERSION );
+require XSLoader;
+XSLoader::load( __PACKAGE__, $VERSION );
 
-    require List::SomeUtils::PP;
+require List::SomeUtils::PP;
 
-    # This list is copied from List::SomeUtils itself and should be updated
-    # when subs are added.
-    my @subs = qw(
-        any all none notall
-        true false
-        firstidx lastidx
-        insert_after insert_after_string
-        apply indexes
-        after after_incl before before_incl
-        firstval lastval
-        each_array each_arrayref
-        pairwise natatime
-        mesh uniq
-        minmax part
-        bsearch
-        sort_by nsort_by
-        one any_u all_u none_u notall_u one_u
-        firstres onlyidx onlyval onlyres lastres
-        singleton bsearchidx
-    );
+# This list is copied from List::SomeUtils itself and should be updated
+# when subs are added.
+my @subs = qw(
+    any all none notall
+    true false
+    firstidx lastidx
+    insert_after insert_after_string
+    apply indexes
+    after after_incl before before_incl
+    firstval lastval
+    each_array each_arrayref
+    pairwise natatime
+    mesh uniq
+    minmax part
+    bsearch
+    sort_by nsort_by
+    one any_u all_u none_u notall_u one_u
+    firstres onlyidx onlyval onlyres lastres
+    singleton bsearchidx
+);
 
-    for my $sub (@subs) {
-        next if __PACKAGE__->can($sub);
-        ## no critic (TestingAndDebugging::ProhibitNoStrict)
-        no strict 'refs';
-        *{$sub} = List::SomeUtils::PP->can($sub);
-    }
+for my $sub (@subs) {
+    next if __PACKAGE__->can($sub);
+    ## no critic (TestingAndDebugging::ProhibitNoStrict)
+    no strict 'refs';
+    *{$sub} = List::SomeUtils::PP->can($sub);
 }
 
 1;
